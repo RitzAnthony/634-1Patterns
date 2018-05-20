e47436500aaa4b0ab6e74ea73748adefff3cb071
RitzAnthony <anthony.ritz@students.hevs.ch>
2018-05-20 18:45:01 +0200

    Patterns of Patterns

---
+++ src/lab12/Product.java
@@ -0,0 +1,15 @@
+package lab12;
+
+/**
+ * Author: Anthony Ritz
+ * Project: 634-1 Patterns
+ * Package: lab12
+ * Date: 5/17/2018
+ */
+public class Product {
+    private int attributeA;
+    private int attributeB;
+    private int attributeC;
+
+
+}
